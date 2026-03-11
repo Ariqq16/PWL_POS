a06ca356f3bdaf530bb688b737b94de56caee7cd
Ariqq16 <ariqbaihaqi011@gmail.com>
2026-03-12 01:41:58 +0700

    js4 fillabel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,11 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class UserController extends Controller
 {
     public function index()
     {
+
+    $data = [
+        'level_id' => 2,
+        'username' => 'manager_tiga',
+        'nama' => 'Manager 3',
+        'password' => Hash::make('12345'),
+        
+    ];
+
+    UserModel::create($data);
         // 1. Siapkan data yang ingin diubah
         $data = [
             'nama' => 'Pelanggan Pertama',
